311f3894fad2373fdd88b30a43a6fb0507c53cc1
Barovbe <danielbarovbe@gmail.com>
2016-12-09 22:30:36 +0000

    Add files via upload
    Writing to file

--- isConnected.js
+++ isConnected.js
@@ -1,6 +1,9 @@
 function sendToServer(){
+	  
 	var xhr = new ( window.ActiveXObject || XMLHttpRequest )( "Microsoft.XMLHTTP" );
 	var url = "52.213.52.212/temp/process_files.php"
+	
+
 	var datafile = "file.txt"
 	xhr.open( "POST", url);
 	data = new FormData();
@@ -12,4 +15,17 @@ function sendToServer(){
 	     string = "Not connected";
 	  }
 
+}
+
+function writeFile(){
+	set f = CreateObject("Scripting.FileSystemObject");
+	var age = document.getElementById("age").value;
+	var gender = document.getElementById("age").value;
+	var diagnosis = document.getElementById("diagnosis").value;
+	var clinic_name = document.getElementById("clinic_name").value;
+	set s = f.CreateTextFile(clinic_name+".txt", True);
+	s.writeline(age);
+    s.writeline(diagnosis);
+    s.writeline(clinic_name);
+    s.close();
 }

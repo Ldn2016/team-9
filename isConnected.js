94fa8aa4fe43557239d960ac1e6aaf1338702794
Barovbe <danielbarovbe@gmail.com>
2016-12-09 22:40:39 +0000

    Write to file for all browsers

--- isConnected.js
+++ isConnected.js
@@ -1,31 +1,31 @@
-function sendToServer(){
+// function sendToServer(){
 	  
-	var xhr = new ( window.ActiveXObject || XMLHttpRequest )( "Microsoft.XMLHTTP" );
-	var url = "52.213.52.212/temp/process_files.php"
+// 	var xhr = new ( window.ActiveXObject || XMLHttpRequest )( "Microsoft.XMLHTTP" );
+// 	var url = "52.213.52.212/temp/process_files.php"
 	
 
-	var datafile = "file.txt"
-	xhr.open( "POST", url);
-	data = new FormData();
-	data.append(datafile,file);
-	try {
-	    xhr.send(data);
-	    string = "Connected";
-	  } catch (error) {
-	     string = "Not connected";
-	  }
+// 	var datafile = "file.txt"
+// 	xhr.open( "POST", url);
+// 	data = new FormData();
+// 	data.append(datafile,file);
+// 	try {
+// 	    xhr.send(data);
+// 	    string = "Connected";
+// 	  } catch (error) {
+// 	     string = "Not connected";
+// 	  }
 
-}
+// }
 
 function writeFile(){
-	set f = CreateObject("Scripting.FileSystemObject");
-	var age = document.getElementById("age").value;
-	var gender = document.getElementById("age").value;
-	var diagnosis = document.getElementById("diagnosis").value;
-	var clinic_name = document.getElementById("clinic_name").value;
-	set s = f.CreateTextFile(clinic_name+".txt", True);
-	s.writeline(age);
-    s.writeline(diagnosis);
-    s.writeline(clinic_name);
-    s.close();
+	text = "kidosjmdosaodsam";
+	var data = new Blob([text], {type: 'text/plain'});
+
+    // If we are replacing a previously generated file we need to
+    // manually revoke the object URL to avoid memory leaks.
+    if (textFile !== null) {
+      window.URL.revokeObjectURL(textFile);
+    }
+
+    textFile = window.URL.createObjectURL(data);
 }

62446a99f737baf1d6055817c2eb930767eb9203
Medardas <zmedardas@gmail.com>
2016-12-10 06:29:56 +0000

    added android main menu

---
+++ androidMenu/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>CMAM</title>
+    <?php include "layout/head.php" ?>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="page-header">
+                    CMAM
+                </h1>
+            </div>
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Choose action</h4>
+                    </div>
+                    <div class="panel-body">
+                        <a href="admit.php" class="btn btn-xlarge btn-primary btn-block">Admit</a><br>
+						<a href="dispatch.php" class="btn btn-xlarge btn-primary btn-block">Discharge</a><br>
+						<a href="stocks.php" class="btn btn-xlarge btn-primary btn-block">Stocks</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ androidMenu/layout/nav.php
@@ -0,0 +1,25 @@
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <p class="navbar-brand">CMAM</p>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+				<li>
+					<a href="index.php">Graphs</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>

46066feaf4eda915e5e5ca2a24e79f1f8333154c
Medardas <zmedardas@gmail.com>
2016-12-10 07:42:29 +0000

    new insertvar

--- mainServer/insertVar.php
+++ mainServer/insertVar.php
@@ -8,19 +8,33 @@ require 'database.php';
 
 $conn = getConnection();
 
-	if($myArray[0] == '1')
+//$arr=implode(", ", $_POST['data']);
+
+	/*if($arr[0] == '1')
 	{
 		$stmt = $conn->prepare("INSERT INTO discharges VALUES(?,?,?,?,?)");
-		$stmt->bind_param("ssisi", $_POST['data'][1], $_POST['data'][2],$_POST['data'][3],$_POST['data'][4],$_POST['data'][5]);
+		$stmt->bind_param("ssisi", $arr[1], $_POST['data'][2],$_POST['data'][3],$_POST['data'][4],$_POST['data'][5]);
 		$stmt->execute();
 	}
 	else
 	{
 		$stmt = $conn->prepare("INSERT INTO admissions VALUES(?,?,?,?,?)");
-		$stmt->bind_param("ssisi", $_POST['data'][1], $_POST['data'][2],$_POST['data'][3],$_POST['data'][4],$_POST['data'][5]);
+		$stmt->bind_param("ssisi", $arr[1], $_POST['data'][2],$_POST['data'][3],$_POST['data'][4],$_POST['data'][5]);
+		$stmt->execute();
+	}*/
+	if($_POST['v0'] == '1')
+	{
+		echo "1";
+		$stmt = $conn->prepare("INSERT INTO discharges VALUES(?,?,?,?,?)");
+		$stmt->bind_param("ssisi", $_POST['v1'], $_POST['v2'],$_POST['v3'],$_POST['v4'],$_POST['v5']);
 		$stmt->execute();
 	}
-}
+	else
+	{
+		echo "2";
+		$stmt = $conn->prepare("INSERT INTO admissions VALUES(?,?,?,?,?)");
+		$stmt->bind_param("ssisi", $_POST['v1'], $_POST['v2'],$_POST['v3'],$_POST['v4'],$_POST['v5']);
+		$stmt->execute();
+	}
 
-fclose($handle);
 ?>
